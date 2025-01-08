139a7198d4e259c0e1568dfb072e6841589ccf05
varunpoloju4 <varunpoloju15@gmail.com>
2025-01-08 13:17:59 +0530

    new files added

---
+++ day4/looping.js
@@ -0,0 +1,40 @@
+//for(let i=0;i<=100;i++){
+//   if(i%2==0){
+//       console.log(i+"is a even number");
+//   }else{
+//    console.log(i+"is a odd number");
+//    }
+//}
+// function factorial(num) {
+//     let result = 1;
+//     for (let i = 1; i <= num; i++) {
+//       result *= i;                          
+//     }
+//     return result;
+//   }
+  
+//   console.log(factorial(5));
+
+//   let num = Number(prompt("enter a number"));
+//   let mul=1;
+//   for(let i=num; i>0; i--){
+//     mul*=i;
+//   }
+//   console.log(mul);
+
+//   let i=1;
+//   while(i<=5){
+//     console.log("Hello wakeup");
+//     i++;
+//   }
+
+// let i=1;
+// do{
+//     i++;
+//     console.log("Hello jey");
+// }while(i>=5);
+ function addToCart(){
+    alert("product is added to cart successfully");
+    document.body.style.background="red";
+ }
+ addToCart()
