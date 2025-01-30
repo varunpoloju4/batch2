953aea98c8f92498cf21d86b72ead2bc6078c58c
varunpoloju4 <varunpoloju15@gmail.com>
2025-01-30 20:44:27 +0530

    day14

---
+++ day14/script2.js
@@ -0,0 +1,13 @@
+import  {collegeName, Tasking as tasking}  from "./script1.js";
+import total from "./script1.js"
+
+tasking();
+console.log(total);
+
+
+//create a signup form that contains username(!empty),email(@. !empty), password(should be 6) and
+//confirm password(same. !empty). once you click on submit button, a alert should be popped up 
+//on you screen "form is submitted successfully" if and only if allthe validations are corect.
+<form>
+    <input type ="text" id="unname" name=""></input>
+</form>
